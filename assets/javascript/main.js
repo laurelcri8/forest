b83bc09d49e93ba6cf67417c7ef0cc953d087873
laurelcri8 <boldabijuwon360@gmail.com>
2022-05-18 18:06:41 +0100

    home page

---
+++ assets/javascript/main.js
@@ -0,0 +1,18 @@
+// -------------------- swiper ------------------
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
+
+let swiperPopular = new Swiper(".popular_container", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+},
+});
