ac88ae1f79cfb7288ea39fc8f12ca7c3d6306487
laurelcri8 <boldabijuwon360@gmail.com>
2022-05-21 16:51:03 +0100

    first commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,18 +1,29 @@
 // -------------------- swiper ------------------
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 3,
-//   spaceBetween: 30,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-// });
-
 let swiperPopular = new Swiper(".popular_container", {
-    slidesPerView: 3,
+    slidesPerView: 4,
     spaceBetween: 30,
+    loop: true,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
 },
+
+// breakPoint: {
+//     709:{
+//         slidesPerView: 3,
+//     }
+// }
+
+breakpoints: {
+    425: {
+      slidesPerView: 2,
+    },
+    940: {
+      spaceBetween: 25,
+      slidesPerView: 4,
+    },
+  },
 });
+
+
+let open_menu= document.getElementById('open_menu')
